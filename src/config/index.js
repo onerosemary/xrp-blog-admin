1d62aa1c9ce69dd2c165ebbda55d898afe91066f
xiangruiping <xiangruiping>
2020-06-14 14:26:41 +0800

    123

--- src/config/index.js
+++ src/config/index.js
@@ -5,16 +5,29 @@ import Vue from 'vue'
 // const hosts = process.env.NODE_ENV === 'development' ? devHosts : process.env.VUE_APP_BASE_API
 
 // 地址写死先, 这个是sw上地址
-// const imgUrl = 'http://localhost:4000/uploads/'
-const imgUrl = 'http://xiangruiping.cn/uploads/'
+let imgUrl =''
+if(process.env.NODE_ENV === 'development'){
+  imgUrl = 'http://localhost:4000/uploads/'
+}else{
+  imgUrl = 'http://xiangruiping.cn/uploads/'
+}
+
+
 
 // 编辑器图片 上传配置
 
 // 图片的base url, 用于拼接后台给的图片
 const IMG_BASE_URL = `${imgUrl}`
+let IMG_UPLOAD_URL = ''
+if(process.env.NODE_ENV === 'development'){
+  // 图片上传地址
+  IMG_UPLOAD_URL = `${process.env.VUE_APP_BASE_API}/article/uploadfile`
+}else{
+  // 图片上传地址
+  IMG_UPLOAD_URL = `${process.env.VUE_APP_BASE_API}article/uploadfile`
+}
 
-// 图片上传地址
-const IMG_UPLOAD_URL = `${process.env.VUE_APP_BASE_API}/article/uploadfile`
+
 
 Vue.prototype.imgUrl = imgUrl
 
